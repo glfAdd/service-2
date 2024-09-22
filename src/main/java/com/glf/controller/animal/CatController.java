a70aac95f117af42b4e02f79940231e80b4a89ba
lg <2239660080@qq.com>
2024-09-22 18:27:13 +0800

    fix

---
+++ src/main/java/com/glf/controller/animal/CatController.java
@@ -0,0 +1,22 @@
+package com.glf.controller.animal;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("cat")
+@Api(tags = "Cat")
+public class CatController {
+    @ApiImplicitParam(name = "name", value = "姓名", required = true)
+    @ApiOperation(value = "向客人问好")
+    @GetMapping("/sayHi")
+    public ResponseEntity<String> sayHi(@RequestParam(value = "name") String name) {
+        return ResponseEntity.ok("Hi:" + name);
+    }
+}
---
+++ src/main/java/com/glf/controller/animal/DogController.java
@@ -0,0 +1,17 @@
+package com.glf.controller.animal;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("dog")
+@Api(tags = "Dog")
+public class DogController {
+
+    @GetMapping("get")
+    public void getDog(){
+
+    }
+}
