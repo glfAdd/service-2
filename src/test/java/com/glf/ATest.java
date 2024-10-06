e674633781da2ce2100b6da5ce8b196e9db67f13
lg <2239660080@qq.com>
2024-10-06 18:35:44 +0800

    fix

---
+++ src/test/java/com/glf/ATest.java
@@ -0,0 +1,9 @@
+//package com.glf;
+//
+//import org.apache.juli.logging.LogFactory;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//public class ATest {
+//    public final Log logger= LogFactory.getLog(this.getClass());
+//}
