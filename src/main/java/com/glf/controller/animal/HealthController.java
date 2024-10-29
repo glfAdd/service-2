9f5b05b8801d3d55fb654449fd7bcec62199da00
gonglongfei <2239660080@qq.com>
2024-10-29 19:45:04 +0800

    fix

---
+++ src/main/java/com/glf/controller/animal/HealthController.java
@@ -0,0 +1,23 @@
+package com.glf.controller.animal;
+
+import io.swagger.annotations.Api;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author glfadd
+ */
+@RestController
+@Api(tags = "HealthCheck")
+public class HealthController {
+    @GetMapping("health")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("health");
+    }
+
+    @GetMapping("health-str")
+    public String healthStr() {
+        return "health-str";
+    }
+}
