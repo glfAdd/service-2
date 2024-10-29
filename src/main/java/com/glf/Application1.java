3966991bc880baa0e229f01040b07e5e16871661
gonglongfei <2239660080@qq.com>
2024-10-29 20:10:56 +0800

    fix

--- src/main/java/com/glf/Application1.java
+++ src/main/java/com/glf/Application1.java
@@ -4,6 +4,9 @@ package com.glf;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author glfadd
+ */
 @SpringBootApplication
 public class Application1 {
     public static void main(String[] args) {
