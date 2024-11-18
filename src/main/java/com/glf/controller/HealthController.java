dc72e030ea0501c678a6096f471a3042bdea847c
gonglongfei <2239660080@qq.com>
2024-11-18 18:02:43 +0800

    fix

--- src/main/java/com/glf/controller/HealthController.java
+++ src/main/java/com/glf/controller/HealthController.java
@@ -1,6 +1,7 @@
 package com.glf.controller;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,14 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
  * @author glfadd
  */
 @RestController
-@Api(tags = "HealthCheck")
+@Api(value = "健康检测", tags = {"健康检测1", "健康检测2"})
 public class HealthController {
     @GetMapping("health")
+    @ApiOperation(value = "检查1", notes = "检查提示1", tags = {"tag1", "tag2"})
     public ResponseEntity<String> health() {
         return ResponseEntity.ok("health");
     }
 
     @GetMapping("/health-str")
+    @ApiOperation(value = "检查2", notes = "检查提示2", tags = {"tag1", "tag2"})
     public String healthStr() {
         return "health-str";
     }
--- src/main/java/com/glf/controller/animal/CatController.java
+++ src/main/java/com/glf/controller/animal/CatController.java
@@ -1,37 +1,49 @@
 package com.glf.controller.animal;
 
-import com.glf.dto.BaseResponse;
-import com.glf.dto.animal.cat.ReqCat2DTO;
-import com.glf.dto.animal.cat.ResCat2DTO;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
+import javax.validation.constraints.Min;
 
 /**
  * @author glfadd
  */
 @RestController
 @RequestMapping("cat")
-@Api(tags = "接收请求解析")
+@Api(tags = "接收请求解析 - CatController")
 public class CatController {
-    @ApiImplicitParam(name = "name", value = "姓名", required = true)
-    @ApiOperation(value = "关键字参数")
+    @ApiOperation(value = "url 参数 (不使用 swagger)")
     @GetMapping("/request1")
-    public ResponseEntity<String> request1(@RequestParam(value = "name") String name) {
-        return ResponseEntity.ok("Hi:" + name);
+    public ResponseEntity<String> request1(@RequestParam(value = "name") String name, @RequestParam(value = "age") Integer age) {
+        return ResponseEntity.ok(String.format("%s - %s", name, age));
     }
 
-    @GetMapping("request2")
-    @ApiOperation(value = "关键字参数-对象解析")
-    public BaseResponse<ResCat2DTO> request2(@Valid ReqCat2DTO reqCatListDTO) {
-        ResCat2DTO res = new ResCat2DTO();
-        return BaseResponse.success(res);
+    @ApiOperation(value = "url 参数 (使用 swagger)")
+    @GetMapping("/request2")
+    public ResponseEntity<String> request2(
+            @ApiParam(name = "name", value = "用户名", defaultValue = "Tom", example = "Luck") @RequestParam(value = "name") String name,
+            @ApiParam(name = "age", required = true, value = "年龄", defaultValue = "18", example = "1") @RequestParam(value = "age") Integer age) {
+        return ResponseEntity.ok(String.format("%s - %s", name, age));
     }
+
+    @ApiOperation(value = "url 参数 (使用 swagger 结合 Valid)")
+    @GetMapping("/request3")
+    public ResponseEntity<String> request3(
+            @ApiParam(name = "name", value = "用户名", required = false, defaultValue = "Tom", example = "Luck") @RequestParam(value = "name", required = false) @Length(max = 10, message = "name 最大长度 10") String name,
+            @ApiParam(name = "age", value = "年龄", required = true, defaultValue = "18", example = "1") @RequestParam(value = "age", required = true) @Min(value = 1, message = "age 必须大于 1") Integer age) {
+        return ResponseEntity.ok(String.format("%s - %s", name, age));
+    }
+//    @ApiOperation(value = "url 参数 (对象解析)")
+//    @GetMapping("request3")
+//    public BaseResponse<ResCat2DTO> request3(@Valid ReqCat2DTO reqCatListDTO) {
+//        ResCat2DTO res = new ResCat2DTO();
+//        return BaseResponse.success(res);
+//    }
 }
