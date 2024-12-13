e8470eae535e691ebdd87231a6333f645e435040
gonglongfei <2239660080@qq.com>
2024-12-13 16:04:35 +0800

    fix

--- src/main/java/com/glf/controller/animal/CatController.java
+++ src/main/java/com/glf/controller/animal/CatController.java
@@ -5,6 +5,7 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.hibernate.validator.constraints.Length;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,6 +19,8 @@ import javax.validation.constraints.Min;
 @RestController
 @RequestMapping("cat")
 @Api(tags = "接收请求解析 - CatController")
+// 启动参数校验
+@Validated
 public class CatController {
     @ApiOperation(value = "url 参数 (不使用 swagger)")
     @GetMapping("/request1")
@@ -28,18 +31,30 @@ public class CatController {
     @ApiOperation(value = "url 参数 (使用 swagger)")
     @GetMapping("/request2")
     public ResponseEntity<String> request2(
-            @ApiParam(name = "name", value = "用户名", defaultValue = "Tom", example = "Luck") @RequestParam(value = "name") String name,
-            @ApiParam(name = "age", required = true, value = "年龄", defaultValue = "18", example = "1") @RequestParam(value = "age") Integer age) {
+            @ApiParam(name = "name", value = "用户名", example = "Luck") @RequestParam(value = "name") String name,
+            @ApiParam(name = "age", required = true, value = "年龄", example = "1") @RequestParam(value = "age") Integer age) {
         return ResponseEntity.ok(String.format("%s - %s", name, age));
     }
 
     @ApiOperation(value = "url 参数 (使用 swagger 结合 Valid)")
     @GetMapping("/request3")
     public ResponseEntity<String> request3(
-            @ApiParam(name = "name", value = "用户名", required = false, defaultValue = "Tom", example = "Luck") @RequestParam(value = "name", required = false) @Length(max = 10, message = "name 最大长度 10") String name,
-            @ApiParam(name = "age", value = "年龄", required = true, defaultValue = "18", example = "1") @RequestParam(value = "age", required = true) @Min(value = 1, message = "age 必须大于 1") Integer age) {
+            @ApiParam(name = "name", value = "用户名", required = false, example = "Tom") @RequestParam(value = "name", required = false) @Length(max = 10, message = "name 最大长度 10") String name,
+            @ApiParam(name = "age", value = "年龄", required = true, example = "18") @RequestParam(value = "age", required = true) @Min(value = 1, message = "age 必须大于 1") Integer age) {
         return ResponseEntity.ok(String.format("%s - %s", name, age));
     }
+
+    @ApiOperation(value = "url 参数 (数组传参)")
+    @GetMapping("/request4")
+    public ResponseEntity<String> request4(
+            @ApiParam(name = "age", value = "年龄", required = true, example = "22,23") @RequestParam(name = "age", required = false) Integer[] age) {
+        for (Integer integer : age) {
+            System.out.println(integer);
+        }
+        return ResponseEntity.ok("123");
+    }
+
+    // 正则表达式参数
 //    @ApiOperation(value = "url 参数 (对象解析)")
 //    @GetMapping("request3")
 //    public BaseResponse<ResCat2DTO> request3(@Valid ReqCat2DTO reqCatListDTO) {
